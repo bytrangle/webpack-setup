72cc837311ac04624d2cef337978c97292798cff
trangthule <lethutrang101@gmail.com>
2020-03-03 21:02:06 +0700

    Add files via upload

---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const path = require("path");
+const port = 5000;
+const app = express();
+
+app.get("/index.js", (req, res) => {
+  res.sendFile(path.join(__dirname + "/../src/index.js"));
+});
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "/index.html"));
+});
+app.listen(port, (request, response) => {
+  console.log(
+    "running server on port " + port + " and the dirname is " + __dirname
+  );
+});
