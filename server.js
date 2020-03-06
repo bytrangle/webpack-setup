5eb6065754239493c2d20814f6111b13790e928b
trangthule <lethutrang101@gmail.com>
2020-03-06 12:13:24 +0700

    Update server.js
    Change route to javascript file from /index.js to /bundle.js

--- server.js
+++ server.js
@@ -3,8 +3,8 @@ const path = require("path");
 const port = 5000;
 const app = express();
 
-app.get("/index.js", (req, res) => {
-  res.sendFile(path.join(__dirname + "/../src/index.js"));
+app.get("/bundle.js", (req, res) => {
+  res.sendFile(path.join(__dirname + "/bundle.js"));
 });
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "/index.html"));
